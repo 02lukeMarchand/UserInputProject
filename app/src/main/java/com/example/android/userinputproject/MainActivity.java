7d4c41c0a5260a54a17760e57e8dff2308d2814d
Luke Marchand <lukemarchandcs@gmail.com>
2021-02-28 21:43:04 -0500

    Created methods for each button to update score

--- app/src/main/java/com/example/android/userinputproject/MainActivity.java
+++ app/src/main/java/com/example/android/userinputproject/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.android.userinputproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayForTeamA(3);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.score_a_textview);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void displayPoints3(){
+        TextView scoreView = (TextView) findViewById(R.id.score_a_textview);
+        scoreView.setText(String.valueOf(3));
+    }
+
+    public void displayPoints2(){
+        TextView scoreView = (TextView) findViewById(R.id.score_a_textview);
+        scoreView.setText(String.valueOf(2));
+    }
+
+    public void displayPoints1(){
+        TextView scoreView = (TextView) findViewById(R.id.score_a_textview);
+        scoreView.setText(String.valueOf(1));
     }
 }
